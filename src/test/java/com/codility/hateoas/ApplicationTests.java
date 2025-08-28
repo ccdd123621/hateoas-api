59bca87427fb648b28a58344b9edaf7f6c0d8315
ccdd123621 <chrissession44@gmail.com>
2025-08-28 15:22:22 -0400

    Create ApplicationTests.java

---
+++ src/test/java/com/codility/hateoas/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.codility.hateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(“test”)
+class ApplicationTests {
+
+```
+@Test
+void contextLoads() {
+// This test ensures that the Spring application context loads successfully
+}
+```
+
+}
